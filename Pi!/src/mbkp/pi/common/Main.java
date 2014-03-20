a8f58000f434767b2b206db0354780d31040205a
drowssaP <maximilianpbloom@gmail.com>
2014-03-20 15:17:39 -0700

    Removed cycling thru array to find console arguments, will replace with something easier later.

--- Pi!/src/mbkp/pi/common/Main.java
+++ Pi!/src/mbkp/pi/common/Main.java
@@ -20,7 +20,7 @@ public class Main extends WritePiToFile{
 	public static void main(String[] args) throws IOException, Exception{
 		
 		String version = "Version 0.6.3";
-		String consoleArgs[] = new String[] {"-version", "-filescreated"};
+		//String consoleArgs[] = new String[] {"-version", "-filescreated"}; unused
 		String filescreated = readFile("C:\\data\\filescreated.txt");
 		
 		//create a BufferedReader using System.in
@@ -28,20 +28,20 @@ public class Main extends WritePiToFile{
 		
 		System.out.println("args are -version, -filescreated, null");
 		System.out.println("How many digits of pi would you like to print?");
-		System.out.println("Maximum is 4679 as of version 0.6.2 \nEDIT: Fixed with version 0.6.3, just cant see in console\nCheck file for full in C:\\data");
+		System.out.println("Maximum is 4679 as of version 0.6.2"
+				+ "\nEDIT: Fixed with version 0.6.3, just cant see in console"
+				+ "\nCheck file for full in C:\\data");
 		rawInput = br.readLine();
 		try{
 			InputFromUser = Integer.parseInt(rawInput);
 		}catch(NumberFormatException exc){
 			
-			if(!rawInput.equals("-version")){
+			if(!rawInput.equals("-version") && !rawInput.equals("-filescreated")){
 			System.out.println("Number Format Exception; Invalid Format." + exc);
 			}
 			
-			if(InputFromUser < 1 ){
-				int n = 0;
-				while(n <= consoleArgs.length) n = n++;
-				if(!rawInput.equals(consoleArgs[n])){
+			if(InputFromUser < 1){
+				if(!rawInput.equals("-version") && !rawInput.equals("-filescreated")){
 					throw new Exception("Input cannot be less than 1!!!");
 				}
 			}
