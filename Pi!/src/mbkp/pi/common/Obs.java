f9873afc3fd4b4258559939828defcd802b0f6a2
drowssaP <maximilianpbloom@gmail.com>
2014-04-09 12:06:27 -0700

    Fixed sychronization issue and added Obs to neaten up files

---
+++ Pi!/src/mbkp/pi/common/Obs.java
@@ -0,0 +1,12 @@
+package mbkp.pi.common;
+
+public class Obs {
+	
+	public static String version = "Version 0.6.3";
+	public static String filedatadir = "C:\\data\\";
+	public static String txtName = "pitothe" + Main.InputFromUser + "thdigit.txt";
+	public static String fileNametxt = filedatadir + txtName;
+	static final String OutputGZIPFile = "";
+	static final String SourceFile = "";
+	
+}
